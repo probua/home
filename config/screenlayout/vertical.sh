c9371bbe1e16fa433a5c9b2466e21452b67b9a38
probua <alejandroraj@gmail.com>
2022-09-14 16:13:10 -0500

    upd: alacritty + i3 + screenlayout

--- config/screenlayout/vertical.sh
+++ config/screenlayout/vertical.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x768 --rotate normal --output DP-0 --off --output DP-1 --mode 1024x768 --pos 0x0 --rotate normal
+xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x768 --rotate normal --output DP-0 --off --output DP-1 --mode 1024x768 --pos 0x0 --rotate normal
