12966c5ed015de420389ee4f9a772906ccc8cc52
probua <alejandroraj@gmail.com>
2022-09-15 02:39:31 -0500

    upd: dmenu_double_confirmation colors

--- config/dmenu/dmenu_double_confirmation.sh
+++ config/dmenu/dmenu_double_confirmation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-dmenumods="-i -fn "Hack-14" -nb "#181818" -nf "#ffd787" -sb "#181818" -sf "#00afff""
+dmenumods="-i -fn "Hack-16" \
+	-nb "#181818" \
+	-nf "#ffd787" \
+	-sb "#ffd787" \
+	-sf "#181818""
 if [ $(echo -e "No\nSi" | dmenu $dmenumods -p "$1") == "Si" ]; then
 	[ $(echo -e "No\nSi" | dmenu $dmenumods -p "$2") == "Si" ] && $3
 fi
