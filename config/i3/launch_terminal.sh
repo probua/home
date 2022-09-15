90615a92d6c7c254d9d28f644954e97dfd19a5fe
probua <alejandroraj@gmail.com>
2022-09-15 18:44:14 -0500

    refac: i3 launch_terminal script

---
+++ config/i3/launch_terminal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+default_terminal=`cat ~/.config/i3/default_terminal`
+$default_terminal
--- set-i3-config.sh
+++ set-i3-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p ~/.config/i3
-ln -s ~/projects/probua/home/config/i3 ~/.config/i3/config
+ln -s ~/projects/probua/home/config/i3/i3 ~/.config/i3/config
+echo "i3-sensible-terminal" > ~/.config/i3/default_terminal
