075353be27e71d585037de3fae001e84383aff9a
probua <alejandroraj@gmail.com>
2022-09-20 02:24:50 -0500

    upd: rofi_wallpaper_picker

--- config/scripts/rofi_wallpaper_picker.sh
+++ config/scripts/rofi_wallpaper_picker.sh
@@ -5,4 +5,6 @@ rofimods="-l 6 -i"
 wallpapers_path="~/projects/probua/wallpapers"
 wallpaper=$(ls ~/projects/probua/wallpapers | grep .jpg | rofi -dmenu $rofimods -p " Wallpaper:")
 
-feh --bg-fill ~/projects/probua/wallpapers/$wallpaper
+if [ "$wallpaper" != "" ]; then
+	feh --bg-fill ~/projects/probua/wallpapers/$wallpaper
+fi
