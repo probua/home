58b905f1a8443a0dc51a0f07bd9bb8aaa6de6b1b
probua <alejandroraj@gmail.com>
2022-09-22 00:17:09 -0500

    add: i3 battery info script

---
+++ config/scripts/i3_battery_info.sh
@@ -0,0 +1,20 @@
+battery_status=$(acpi | awk -F ", " '{print $1}' $1 | awk -F " " '{print $3}' $1)
+battery_perc=$(acpi | awk -F ", " '{print $2}' $1)
+
+battery_icon=" "
+
+if [[ "$battery_status" == "Discharging" ]]; then
+	if [[ "$battery_perc" > "90" ]]; then
+		battery_icon="  "
+	elif [[ "$battery_perc" > "66" ]]; then
+		battery_icon="  "
+	elif [[ "$battery_perc" > "33" ]]; then
+		battery_icon="  "
+	elif [[ "$battery_perc" > "10" ]]; then
+		battery_icon="  "
+	else
+		battery_icon="  "
+	fi
+fi
+
+echo "$battery_icon  $battery_perc"
