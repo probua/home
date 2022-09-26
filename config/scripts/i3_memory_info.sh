a856a2e8c8da654b254e48a7655a0620e350fa3d
probua <alejandroraj@gmail.com>
2022-09-26 15:49:02 -0500

    add: i3 script memory_info

---
+++ config/scripts/i3_memory_info.sh
@@ -0,0 +1,5 @@
+icon=" "
+total=($(free | awk -F "Mem:" '{print $2}' | awk -F " " '{print $1}'))
+used=($(free | awk -F "Mem:" '{print $2}' | awk -F " " '{print $2}'))
+
+echo "$icon" "$total" "$used" | awk '{ printf("%s  %.2f%%\n"), $1, $3/$2*100 }'
