3c88b35b34cb2318f2cc3661ace9adf4598f4cb0
probua <alejandroraj@gmail.com>
2022-10-18 12:55:45 -0500

    upd: i3blocks sound icons

--- config/scripts/i3_mic.sh
+++ config/scripts/i3_mic.sh
@@ -1,9 +1,12 @@
 ICON=""
+MIC_OFF=""
 OFF=" "
 ON=" "
 
 if [[ "$(pactl get-source-mute @DEFAULT_SOURCE@)" != "Mute: no" ]]; then
-	echo "$ICON $OFF"
+#	echo "$ICON $OFF"
+	echo "$MIC_OFF"
 else
-	echo "$ICON $ON"
+#	echo "$ICON $ON"
+	echo "$ICON"
 fi
--- config/scripts/i3_sound_mute.sh
+++ config/scripts/i3_sound_mute.sh
@@ -3,10 +3,13 @@
 OFF=" "
 ON=" "
 ICON=""
+SOUND_MUTED=""
 MUTED=$(amixer get Master | awk ' /%/{print ($NF=="[off]" ? 1 : 0); exit;}')
 
 if [ "$MUTED" = "1" ]; then
-	echo "$ICON $OFF" 
+#	echo "$ICON $OFF" 
+	echo "$SOUND_MUTED"
 else
-	echo "$ICON $ON"
+#	echo "$ICON $ON"
+	echo "$ICON"
 fi
