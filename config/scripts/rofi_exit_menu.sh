0623b25bf79cd54131907995a263d74e95149c35
probua <alejandroraj@gmail.com>
2023-04-15 09:34:40 -0500

    upd: i3 + rofi_exit_menu

--- config/scripts/rofi_exit_menu.sh
+++ config/scripts/rofi_exit_menu.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-rofimods="-l 3 -i"
+rofimods="-l 4 -i"
 
-rofi_select=$(echo -e "    Lock screen\n  Close session\n    Shutdown" | rofi -dmenu $rofimods -p "Exit:")
+rofi_select=$(echo -e " Lock screen\n Close session\n Reboot \n Shutdown" | rofi -dmenu $rofimods -p "Exit:")
 if [[ "$rofi_select" != "" ]]; then
 	if [[ $rofi_select == *"Lock screen"* ]]; then
-			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh "  Confirm lock screen:" "dm-tool lock"
+			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh " Confirm lock screen:" "dm-tool lock"
 	elif [[ $rofi_select == *"Close session"* ]]; then
-			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh "  Confirm close session:" "i3-msg exit"
+			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh " Confirm close session:" "i3-msg exit"
+	elif [[ $rofi_select == *"Reboot"* ]]; then
+			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh " Confirm reboot:" "reboot"
 	elif [[ $rofi_select == *"Shutdown"* ]]; then
-			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh "  Confirm shutdown:" "shutdown now"
+			source ~/projects/probua/home/config/scripts/rofi_single_confirmation.sh " Confirm shutdown:" "shutdown now"
 	fi
 fi
 
