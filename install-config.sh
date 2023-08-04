04b19cd1b0996ef878f26b6f36c9478f1a300757
probua <alejandroraj@gmail.com>
2023-08-04 00:05:13 -0500

    add: install-config.sh

---
+++ install-config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source config/scripts/set-bash-config.sh
+source config/scripts/set-vim-config.sh
+source config/scripts/set-rofi-config.sh
+source config/scripts/set-picom-config.sh
+source config/scripts/set-i3-config.sh
+source config/scripts/set-alacritty-config.sh
