c21d9c5e845a4b096744fc6bca0e88d14eb8394b
probua <alejandroraj@gmail.com>
2023-08-22 13:50:03 -0500

    upd: config/scripts/i3-sound-level.sh

--- config/scripts/i3-sound-level.sh
+++ config/scripts/i3-sound-level.sh
@@ -6,9 +6,9 @@ VOLUME_HIGH=""
 SOUND_LEVEL=$(amixer -M get Master | awk -F"[][]" '/%/ { print $2 }' | awk -F"%" 'BEGIN{tot=0; i=0} {i++; tot+=$1} END{printf("%s\n", tot/i) }')
 
 ICON="$VOLUME_0"
-if [ "$SOUND_LEVEL" -lt 10 ]; then
+if [ "$SOUND_LEVEL" -lt 5 ]; then
 	ICON="$VOLUME_0"
-elif [ "$SOUND_LEVEL" -lt 40 ]; then
+elif [ "$SOUND_LEVEL" -lt 50 ]; then
 	ICON="$VOLUME_LOW"
 else
 	ICON="$VOLUME_HIGH"
