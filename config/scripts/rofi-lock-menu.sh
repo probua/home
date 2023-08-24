04d4ae7cb83a5ab8f9845f042ce072513c9b7391
probua <alejandroraj@gmail.com>
2023-08-24 13:59:59 -0500

    add: i3-lock.sh

--- config/scripts/rofi-lock-menu.sh
+++ config/scripts/rofi-lock-menu.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-source ~/.config/probua/scripts/rofi-single-confirmation.sh "Confirm lock screen:" "dm-tool lock"
+source ~/.config/probua/scripts/rofi-single-confirmation.sh "Confirm lock screen:" "source $HOME/.config/probua/scripts/i3-lock.sh"
+
