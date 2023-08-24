e25ec5fd2027440ac72da9a75e94de85ebcf18eb
probua <alejandroraj@gmail.com>
2023-08-24 14:20:50 -0500

    upd: scripts/rofi-exit-menu.sh

--- config/scripts/rofi-exit-menu.sh
+++ config/scripts/rofi-exit-menu.sh
@@ -4,7 +4,7 @@ rofimods="-l 4 -i"
 rofi_select=$(echo -e "Lock screen\nClose session\nReboot \nShutdown" | rofi -dmenu $rofimods -p "Exit")
 if [[ "$rofi_select" != "" ]]; then
 	if [[ $rofi_select == *"Lock screen"* ]]; then
-			source ~/.config/probua/scripts//rofi-single-confirmation.sh "Confirm lock screen" "dm-tool lock"
+			source ~/.config/probua/scripts//rofi-single-confirmation.sh "Confirm lock screen" "source $HOME/.config/probua/scripts/i3-lock.sh"
 	elif [[ $rofi_select == *"Close session"* ]]; then
 			source ~/.config/probua/scripts//rofi-single-confirmation.sh "Confirm close session" "i3-msg exit"
 	elif [[ $rofi_select == *"Reboot"* ]]; then
