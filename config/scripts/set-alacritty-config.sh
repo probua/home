0fca0bd51a667f13d8faa69f47c7198abbf0484b
probua <alejandroraj@gmail.com>
2024-06-27 21:02:28 -0500

    upd: alacritty configuration (.yml/.toml)

--- config/scripts/set-alacritty-config.sh
+++ config/scripts/set-alacritty-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cat config/alacritty > ~/.alacritty.yml
+cat config/alacritty/alacritty.yml > ~/.alacritty.yml
+cat config/alacritty/alacritty.toml > ~/.alacritty.toml
 echo "alacritty" > ~/.config/i3/terminal
