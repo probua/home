3bc91ec46888023666b908531136ddea839978cb
probua <alejandroraj@gmail.com>
2024-08-13 12:48:51 -0500

    upd: ubuntu/autoinstaller

--- config/ubuntu/install-kmonad.sh
+++ config/ubuntu/install-kmonad.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/kmonad/kmonad/releases/download/0.4.2/kmonad
-sudo mv ./kmonad /usr/bin/kmonad
-sudo chmod +x /usr/bin/kmonad
+mv ./kmonad /usr/bin/kmonad
+chmod +x /usr/bin/kmonad
