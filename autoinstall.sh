a0558ca5ecad5ecef2d29afb0bbd258686724141
probua <alejandroraj@gmail.com>
2024-08-16 00:12:02 -0500

    upd: autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo curl -o- -L https://slss.io/install | bash
-sudo source config/scripts/auto-install/install-kmonad.sh
-sudo source config/scripts/auto-install/install-docker.sh
+sudo curl -o- -L https://slss.io/install | bash # install serverless
+sudo source config/scripts/auto-install/install-kmonad.sh # install kmonad
+sudo source config/scripts/auto-install/install-docker.sh # install docker
 sudo usermod -aG docker probua
 sudo usermod -aG video probua
 source install-config.sh
 sudo timedatectl set-timezone America/Lima
+sudo passwd probua # update password
