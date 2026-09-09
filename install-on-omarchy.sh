3daf1536c592d2fb0ff0de372786516463d25ce2
Alejandro Aguilar <alejandroraj@gmail.com>
2026-09-08 22:20:21 -0500

    upd: bash alias + install-on-omarchy

---
+++ install-on-omarchy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source config/scripts/set-bash-config.sh
+source config/scripts/set-vim-config.sh
+# source config/scripts/set-rofi-config.sh
+# source config/scripts/set-picom-config.sh
+# source config/scripts/set-i3-config.sh
+# source config/scripts/set-alacritty-config.sh
+# source config/scripts/set-kmonad-config.sh
+# source config/scripts/set-autohotkey-config.sh
